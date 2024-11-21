db69cfc345324d9c082d1d49c2069511de9b0328
Ragas30 <130697528+Ragas30@users.noreply.github.com>
2024-11-21 23:32:35 +0700

    modif produk controller and routes

--- src/controllers/produkController.js
+++ src/controllers/produkController.js
@@ -1,4 +1,5 @@
-import { Prisma } from "@prisma/client";
+import { kategori } from "../routes/kategoriRoute.js";
+import { Prisma } from "../utils/dataBase.js";
 
 const prisma = new Prisma.Client();
 
@@ -11,6 +12,12 @@ export const createProduk = async (req, res) => {
         namaProduk: namaProduk,
         jumlah: jumlah,
         deskripsi: deskripsi,
+      },
+      select: {
+        kategoriId: true,
+        namaProduk: true,
+        jumlah: true,
+        deskripsi: true,
       },
     });
     res.status(201).json({
@@ -37,7 +44,78 @@ export const getProduk = async (req, res) => {
 
 export const getProdukById = async (req, res) => {
   try {
-    const produk = await prisma.produk.findUnique();
+    const produk = await prisma.produk.findUnique({
+      where: { id: Number(req.params.id) },
+    });
+    if (!produk) {
+      res.status(404).json({ message: "Produk tidak ditemukan" });
+    }
+    res.status(200).json({
+      data: produk,
+    });
+  } catch (error) {
+    res.status(500).json({ errors: error.message });
+  }
+};
+
+export const updateProduk = async (req, res) => {
+    const { kategoriId, namaProduk, jumlah, deskripsi } = req.body;
+  try {
+    let produk = await prisma.produk.update({
+      where: { id: Number(req.params.id) },
+      data: {
+        kategoriId: kategoriId,
+        namaProduk: namaProduk,
+        jumlah: jumlah,
+        deskripsi: deskripsi,
+      },
+    });
+    if (!produk) {
+      res.status(404).json({ message: "Produk tidak ditemukan" });
+    }
+    produk = await prisma.produk.findUnique({
+      where: { id: Number(req.params.id) },
+      data: {
+        kategoriId: kategoriId,
+        namaProduk: namaProduk,
+        jumlah: jumlah,
+        deskripsi: deskripsi,
+      }
+    })
+    res.status(200).json({
+        message: "Produk Dimodif ges",
+      data: produk,
+    });
+  } catch (error) {
+    res.status(500).json({ errors: error.message });
+  }
+};
+
+export const deleteProduk = async (req, res) => {
+  try {
+    const produk = await prisma.produk.delete();
+    res.status(200).json({
+      data: produk,
+    });
+  } catch (error) {
+    res.status(500).json({ errors: error.message });
+  }
+};
+
+export const getProdukByKategori = async (req, res) => {
+  try {
+    const produk = await prisma.produk.findMany();
+    res.status(200).json({
+      data: produk,
+    });
+  } catch (error) {
+    res.status(500).json({ errors: error.message });
+  }
+};
+
+export const getProdukByKategoriId = async (req, res) => {
+  try {
+    const produk = await prisma.produk.findMany();
     res.status(200).json({
       data: produk,
     });
--- src/routes/produkRoutes.js
+++ src/routes/produkRoutes.js
@@ -3,3 +3,6 @@ import { getProduk } from "../controllers/produkController";
 export const produk = express.Router();
 produk.post("/produk", createProduk);
 produk.get("/produk", getProduk);
+produk.get("/produk/:id", getProdukById);
+produk.put("/produk/:id", updateProduk);
+produk.delete("/produk/:id", deleteProduk);
